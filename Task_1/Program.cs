b920ceadccc98fe7f56240f0d82bc01b7506177d
MladjanMrksic <mladjanmrksic@gmail.com>
2020-06-26 12:41:35 +0200

    Added Barrier so that the program ends when all threads are done. Created a method to colour code the text for easier reading. Added embellishments.

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -10,7 +10,8 @@ namespace Task_1
 {
     class Program
     {
-        public static List<string> ColourList = new List<string> { "Yellow", "Red", "Blue", "Green", "Orange", "Pink", "Purple", "Brown", "White", "LightBlue" };
+        static Barrier bar = new Barrier(11);
+        public static List<string> ColourList = new List<string> { "Yellow", "Red", "Blue", "Green", "Magenta", "Cyan", "DarkYellow", "DarkGreen", "White", "DarkBlue" };
         public static Random rnd = new Random();
         public static List<string> Orientation = new List<string> { "Landscape", "Portrait" };
         static string[] Formats = new string[] { "A3", "A4" };
@@ -21,19 +22,18 @@ namespace Task_1
         static Program pr = new Program();
         static void Main(string[] args)
         {
-
+            Console.WriteLine("\t\t\tWelcome to Printer Simulation!");
             pr.CreateColours();
-
-            Thread.Sleep(1000);
             for (int i = 1; i < 11; i++)
             {
+                Thread.Sleep(100);
                 string format = Formats[rnd.Next(0, 2)];
                 if (format == "A3")
                 {
                     PrintRequest pr = new PrintRequest(new Thread(A3Printer));
                     pr.Orientation = Orientation[rnd.Next(0, 2)];
                     pr.T.Name = string.Format("Computer " + i);
-                    pr.T.Start(pr);
+                    pr.T.Start(pr);                    
                 }
                 else if (format == "A4")
                 {
@@ -43,6 +43,8 @@ namespace Task_1
                     pr.T.Start(pr);
                 }
             }
+            bar.SignalAndWait();
+            Console.WriteLine("\n\t\t\t All requests have been processed!");
             Console.ReadLine();
         }
         public static void A3Printer(object Request)
@@ -50,22 +52,32 @@ namespace Task_1
             lock (A3Lock)
             {                  
                 PrintRequest pr = (PrintRequest)Request;
-                Console.WriteLine(pr.T.Name + " sent print request for document in A3 format.\nColour: " + pr.Colour + ".\nOrientation: " + pr.Orientation);
+                TextcColour(pr.Colour);
+                Console.WriteLine("\n" + pr.T.Name + " sent print request for document in A3 format.\nColour: " + pr.Colour + ".\nOrientation: " + pr.Orientation);
+                Console.ResetColor();
                 Thread.Sleep(1000);
-                Console.WriteLine("User of " + pr.T.Name + " can collect their document in A3 format.");
+                TextcColour(pr.Colour);
+                Console.WriteLine("\n\tUser of " + pr.T.Name + " can collect their document in A3 format.");
+                Console.ResetColor();
                 Monitor.Pulse(A3Lock);
             }
+            bar.SignalAndWait();
         }
         public static void A4Printer(object Request)
         {
             lock (A4Lock)
             {
                 PrintRequest pr = (PrintRequest)Request;
-                Console.WriteLine(pr.T.Name + " sent print request for document in A4 format.\nColour: " + pr.Colour + ".\nOrientation: " + pr.Orientation);
+                TextcColour(pr.Colour);
+                Console.WriteLine("\n" + pr.T.Name + " sent print request for document in A4 format.\nColour: " + pr.Colour + ".\nOrientation: " + pr.Orientation);
+                Console.ResetColor();
                 Thread.Sleep(1000);
-                Console.WriteLine("User of " + pr.T.Name + " can collect their document in A4 format.");
+                TextcColour(pr.Colour);
+                Console.WriteLine("\n\tUser of " + pr.T.Name + " can collect their document in A4 format.");
+                Console.ResetColor();
                 Monitor.Pulse(A4Lock);
             }
+            bar.SignalAndWait();
         }
         public void CreateColours()
         {
@@ -79,6 +91,58 @@ namespace Task_1
                 {
                     sw.WriteLine(colour);
                 }
+            }
+        }
+        public static void TextcColour(string colour)
+        {
+            if (colour == "Yellow")
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+            }
+            else if (colour == "Red")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+
+            }
+            else if (colour == "Blue")
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+
+            }
+            else if (colour == "Green")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+
+            }
+            else if (colour == "Magenta")
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+
+            }
+            else if (colour == "Cyan")
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+
+            }
+            else if (colour == "DarkYellow")
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            }
+            else if (colour == "DarkGreen")
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            }
+            else if (colour == "White")
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+
+            }
+            else if (colour == "DarkBlue")
+            {
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+
             }
         }
     }
